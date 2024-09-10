41c9152ddd31b8827c278d72f466dbd710079dd4
bruhmagedon <schubert.walk@yandex.ru>
2024-09-10 14:16:18 +0300

    AuthPage

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
+import { Label } from "@/shared/ui/label";
+
 export default async function Auth() {
   return (
     <div className='flex h-full items-center justify-center'>
-      <div className='h-[60%] w-[60%] rounded-lg bg-slate-300'></div>
+      <div className='flex h-[60%] w-[80%] rounded-lg bg-slate-400 p-4'>
+        {/* Левый блок */}
+        <div className='flex w-1/2 flex-col items-center justify-center gap-10 bg-slate-200'>
+          <h2>goat logistic</h2>
+          <div className='grid w-full max-w-sm items-center gap-1.5'>
+            <Label htmlFor='email'>Email</Label>
+            <Input type='email' id='email' placeholder='Введите свой email' />
+          </div>
+          <div className='grid w-full max-w-sm items-center gap-1.5'>
+            <Label htmlFor='password'>Пароль</Label>
+            <Input type='password' id='password' placeholder='Введите пароль' />
+          </div>
+          <Button type='submit' className='bg-white'>
+            Войти
+          </Button>
+        </div>
+        <hr className='h-full w-0.5 bg-slate-400' />
+        {/* Правый блок */}
+        <div className='w-1/2'></div>
+      </div>
     </div>
   );
 }
