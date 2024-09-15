8d1ce0c31e51c5307e7e7187823b8e56968ea7ec
bruhmagedon <schubert.walk@yandex.ru>
2024-09-15 14:38:37 +0300

    fix loginform & container

--- frontend/components/login-form/LoginForm.tsx
+++ frontend/components/login-form/LoginForm.tsx
@@ -12,16 +12,16 @@ import { cn } from "@/lib/utils";
 const authFormConfig = {
   login: {
     formTitle: "Вход",
-    buttonText: "Войти"
+    buttonText: "Войти",
   },
   register_person: {
     formTitle: "Регистрация",
-    buttonText: "Зарегистрироваться"
+    buttonText: "Зарегистрироваться",
   },
   register_organization: {
     formTitle: "Регистрация",
-    buttonText: "Зарегистрироваться"
-  }
+    buttonText: "Зарегистрироваться",
+  },
 };
 
 type LoginFormTypes = "login" | "register_person" | "register_organization";
@@ -40,14 +40,18 @@ export const AuthForm = () => {
   };
 
   return (
-    <div className='flex flex-col items-center justify-center gap-5'>
-      <div className='mb-10 flex flex-col items-center gap-3'>
-        <h2 className='text-xl font-semibold'>{formTitle}</h2>
+    <div className="flex flex-col items-center justify-center gap-5">
+      <div className="mb-10 flex flex-col items-center gap-3">
+        <h2 className="text-xl font-semibold">{formTitle}</h2>
         {/* <h3 className='text-sm font-light'>Введите ваши данные</h3> */}
       </div>
-      {form === "login" && <LoginForm onChangeForm={onChangeForm} onAuth={onAuth} />}
+      {form === "login" && (
+        <LoginForm onChangeForm={onChangeForm} onAuth={onAuth} />
+      )}
       {form === "register_person" && <PersonForm onChangeForm={onChangeForm} />}
-      {form === "register_organization" && <OrganizationForm onChangeForm={onChangeForm} />}
+      {form === "register_organization" && (
+        <OrganizationForm onChangeForm={onChangeForm} onAuth={onAuth} />
+      )}
     </div>
   );
 };
@@ -60,13 +64,23 @@ interface LoginFormProps {
 const LoginForm = ({ onChangeForm, onAuth }: LoginFormProps) => {
   return (
     <>
-      <InputForm name='Email' type='email' placeholder='Введите почту' />
-      <InputForm name='Пароль' type='password' placeholder='Введите пароль' className='mb-5' />
-      <div className='flex flex-col gap-5'>
-        <Button type='submit' variant={"secondary"} onClick={onAuth} text='Войти' />
+      <InputForm name="Email" type="email" placeholder="Введите почту" />
+      <InputForm
+        name="Пароль"
+        type="password"
+        placeholder="Введите пароль"
+        className="mb-5"
+      />
+      <div className="flex flex-col gap-5">
+        <Button
+          type="submit"
+          variant={"secondary"}
+          onClick={onAuth}
+          text="Войти"
+        />
         <a
-          href='#'
-          className='text-sm hover:underline'
+          href="#"
+          className="text-sm hover:underline"
           onClick={() => onChangeForm("register_person")}
         >
           Регистрация
@@ -79,19 +93,28 @@ const LoginForm = ({ onChangeForm, onAuth }: LoginFormProps) => {
 const PersonForm = ({ onChangeForm }: LoginFormProps) => {
   return (
     <>
-      <InputForm name='Email' type='email' placeholder='Введите почту' />
-      <InputForm name='Имя' type='text' placeholder='Введите имя' />
-      <InputForm name='Пароль' type='password' placeholder='Введите пароль' className='mb-5' />
+      <InputForm name="Email" type="email" placeholder="Введите почту" />
+      <InputForm name="Имя" type="text" placeholder="Введите имя" />
+      <InputForm
+        name="Пароль"
+        type="password"
+        placeholder="Введите пароль"
+        className="mb-5"
+      />
 
-      <div className='flex w-full items-center justify-between'>
-        <Button type='button' onClick={() => onChangeForm("login")} variant={"secondary"}>
+      <div className="flex w-full items-center justify-between">
+        <Button
+          type="button"
+          onClick={() => onChangeForm("login")}
+          variant={"secondary"}
+        >
           <ArrowLeft size={20} />
         </Button>
         <Button
-          type='button'
+          type="button"
           onClick={() => onChangeForm("register_organization")}
           variant={"secondary"}
-          text='Ваша организация'
+          text="Ваша организация"
           suffix={<ArrowRight size={20} />}
         />
       </div>
@@ -99,27 +122,36 @@ const PersonForm = ({ onChangeForm }: LoginFormProps) => {
   );
 };
 
-const OrganizationForm = ({ onChangeForm }: LoginFormProps) => {
+const OrganizationForm = ({ onChangeForm, onAuth }: LoginFormProps) => {
   return (
     <>
       <InputForm
-        name='Название организации'
-        type='email'
-        placeholder='Введите название организации'
+        name="Название организации"
+        type="email"
+        placeholder="Введите название организации"
       />
-      <InputForm name='ИНН' type='text' placeholder='Введите ИНН организации' />
+      <InputForm name="ИНН" type="text" placeholder="Введите ИНН организации" />
       <InputForm
-        name='Адрес'
-        type='text'
-        placeholder='Введите адрес организации'
-        className='mb-5'
+        name="Адрес"
+        type="text"
+        placeholder="Введите адрес организации"
+        className="mb-5"
       />
 
-      <div className='flex w-full items-center justify-between'>
-        <Button type='button' onClick={() => onChangeForm("register_person")} variant={"secondary"}>
+      <div className="flex w-full items-center justify-between">
+        <Button
+          type="button"
+          onClick={() => onChangeForm("register_person")}
+          variant={"secondary"}
+        >
           <ArrowLeft size={20} />
         </Button>
-        <Button type='button' variant={"secondary"} text='Зарегистрироваться' />
+        <Button
+          type="button"
+          variant={"secondary"}
+          text="Зарегистрироваться"
+          onClick={onAuth}
+        />
       </div>
     </>
   );
@@ -133,13 +165,13 @@ interface InputFormProps {
 }
 const InputForm = ({ name, type, placeholder, className }: InputFormProps) => {
   return (
-    <div className={cn("grid w-full max-w-sm items-center gap-1.5", className)}>
+    <div className={cn("grid w-full max-w-sm items-center gap-2.5", className)}>
       <Label htmlFor={`${name}__${type}`}>{name}</Label>
       <Input
         type={type}
         id={`${name}__${type}`}
         placeholder={placeholder}
-        className='h-10 w-[300px] px-3'
+        className="h-10 w-[300px] px-3"
       />
     </div>
   );
